2e715a60a282a4ff0e2697e72042449f4e6968cb
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-27 14:04:02 -0500

    Merged the Create and Update patient DTOS

---
+++ src/main/java/co/com/hospital/domain/dto/patient/CreateAndUpdatePatientDTO.java
@@ -0,0 +1,33 @@
+package co.com.hospital.domain.dto.patient;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CreateAndUpdatePatientDTO {
+    private Long id;
+    private String dni;
+    private String fullName;
+    private Integer age;
+
+    private boolean idIsNull() {
+        return this.getId() != null;
+    }
+
+    public boolean isValidCreationDTO() {
+        if (!this.idIsNull()) {
+            throw new IllegalArgumentException("The given DTO shouldn't have an Id field; It will be automatically assigned by the database.");
+        }
+        return true;
+    }
+
+    public boolean isValidUpdateDTO() {
+        if (this.idIsNull()) {
+            throw new IllegalArgumentException("The given DTO does not have an Id field; You should supply one.");
+        }
+        return true;
+    }
+}
