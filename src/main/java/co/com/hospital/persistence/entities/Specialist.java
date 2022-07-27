8554265741d02ba4d296fc749951b9dcaa31126c
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-27 10:57:55 -0500

    Added isAvailable and no-id constructor on Specialist

--- src/main/java/co/com/hospital/persistence/entities/Specialist.java
+++ src/main/java/co/com/hospital/persistence/entities/Specialist.java
@@ -15,4 +15,14 @@ public class Specialist extends Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "is_available", nullable = false)
+    private Boolean isAvailable;
+
+    // TODO add relationship to associated department.
+
+    public Specialist(String DNI, String fullName, Integer age, Boolean isAvailable) {
+        super(DNI, fullName, age);
+        this.isAvailable = isAvailable;
+    }
 }
