cc24b006ae4867f45578ad9e28b28ca7251e66f3
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-27 12:08:47 -0500

    Added the GetPatient DTO

---
+++ src/main/java/co/com/hospital/domain/dto/patient/GetPatientDTO.java
@@ -0,0 +1,11 @@
+package co.com.hospital.domain.dto.patient;
+
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString(callSuper = true)
+@NoArgsConstructor
+public class GetPatientDTO extends GetPartialPatientDTO {
+    // TODO add the clinical history DTO.
+    // TODO add all-args constructor that calls super().
+}
