a0ed8a283043f7a2b7d6764dd332e96eae521ab3
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-28 08:53:40 -0500

    Added a method to find a clinical history given its associated patient id

--- src/main/java/co/com/hospital/persistence/repository/ClinicalHistoryRepository.java
+++ src/main/java/co/com/hospital/persistence/repository/ClinicalHistoryRepository.java
@@ -2,6 +2,13 @@ package co.com.hospital.persistence.repository;
 
 import co.com.hospital.persistence.entities.ClinicalHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ClinicalHistoryRepository extends JpaRepository<ClinicalHistory, String> {
+    @Query(
+            "select history from ClinicalHistory history where history.associatedPatient.id = ?1"
+    )
+    Optional<ClinicalHistory> findByAssociatedPatientId(Long id);
 }
