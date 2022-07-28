d2b830d9d6a5277e0344ce0b0b5fef226d891cd7
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-28 10:46:47 -0500

    Added findAllByIsAvailableTrue on SpecialistRepository

--- src/main/java/co/com/hospital/persistence/repository/SpecialistRepository.java
+++ src/main/java/co/com/hospital/persistence/repository/SpecialistRepository.java
@@ -3,5 +3,8 @@ package co.com.hospital.persistence.repository;
 import co.com.hospital.persistence.entities.Specialist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SpecialistRepository extends JpaRepository<Specialist, Long> {
+    List<Specialist> findAllByIsAvailableTrue();
 }
