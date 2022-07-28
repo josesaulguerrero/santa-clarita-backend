82a8b7a854a58f143956dd475b023244d43a8a0b
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-28 07:58:08 -0500

    Id is not necessary on Create-Dtos

--- src/main/java/co/com/hospital/domain/dto/appointment/CreateAppointmentDTO.java
+++ src/main/java/co/com/hospital/domain/dto/appointment/CreateAppointmentDTO.java
@@ -1,6 +1,5 @@
 package co.com.hospital.domain.dto.appointment;
 
-import co.com.hospital.utils.RuntimeExceptionBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,21 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Data
 public class CreateAppointmentDTO {
-    private Long id;
     private LocalDateTime date;
     private Long clinicalHistoryId;
     private Long specialtyId;
-
-    private boolean idIsNull() {
-        return this.getId() != null;
-    }
-
-    public boolean isValidCreationDTO() {
-        if (!this.idIsNull()) {
-            throw new RuntimeExceptionBuilder(IllegalArgumentException.class)
-                    .developerMessage("The given DTO shouldn't have an Id field; It will be automatically assigned by the database.")
-                    .build();
-        }
-        return true;
-    }
 }
--- src/main/java/co/com/hospital/domain/dto/patient/CreatePatientDTO.java
+++ src/main/java/co/com/hospital/domain/dto/patient/CreatePatientDTO.java
@@ -1,27 +1,12 @@
 package co.com.hospital.domain.dto.patient;
 
-import co.com.hospital.utils.RuntimeExceptionBuilder;
 import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class CreatePatientDTO {
-    private Long id;
     private String dni;
     private String fullName;
     private Integer age;
-
-    private boolean idIsNull() {
-        return this.getId() != null;
-    }
-
-    public boolean isValidCreationDTO() {
-        if (!this.idIsNull()) {
-            throw new RuntimeExceptionBuilder(IllegalArgumentException.class)
-                    .developerMessage("The given DTO shouldn't have an Id field; It will be automatically assigned by the database.")
-                    .build();
-        }
-        return true;
-    }
 }
