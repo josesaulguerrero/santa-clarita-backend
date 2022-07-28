a25c71e148b80d3238767151c36f8f36792ce599
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-27 19:58:32 -0500

    Updated the DetailedAppointment DTO class

--- src/main/java/co/com/hospital/domain/dto/appointment/DetailedAppointmentDTO.java
+++ src/main/java/co/com/hospital/domain/dto/appointment/DetailedAppointmentDTO.java
@@ -1,13 +1,20 @@
 package co.com.hospital.domain.dto.appointment;
 
+import co.com.hospital.domain.dto.specialty.PartialSpecialtyDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @ToString(callSuper = true)
 @NoArgsConstructor
 @Getter
 public class DetailedAppointmentDTO extends PartialAppointmentDTO {
-    //TODO add the specialty partial DTO.
-    //TODO add all-args constructor that calls super().
+    private PartialSpecialtyDTO specialty;
+
+    public DetailedAppointmentDTO(Long id, LocalDateTime date, PartialSpecialtyDTO specialty) {
+        super(id, date);
+        this.specialty = specialty;
+    }
 }
