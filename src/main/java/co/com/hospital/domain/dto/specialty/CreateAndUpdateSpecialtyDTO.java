8d7b24cc58076949dc5c2ebfe2305ddf086459d6
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-27 19:55:43 -0500

    Created the CreateAndUpdateSpecialty DTO

---
+++ src/main/java/co/com/hospital/domain/dto/specialty/CreateAndUpdateSpecialtyDTO.java
@@ -0,0 +1,37 @@
+package co.com.hospital.domain.dto.specialty;
+
+import co.com.hospital.utils.RuntimeExceptionBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CreateAndUpdateSpecialtyDTO {
+    protected Long id;
+    protected String name;
+    private Long specialistId;
+
+    private boolean idIsNull() {
+        return this.getId() != null;
+    }
+
+    public boolean isValidCreationDTO() {
+        if (!this.idIsNull()) {
+            throw new RuntimeExceptionBuilder(IllegalArgumentException.class)
+                    .developerMessage("The given DTO shouldn't have an Id field; It will be automatically assigned by the database.")
+                    .build();
+        }
+        return true;
+    }
+
+    public boolean isValidUpdateDTO() {
+        if (this.idIsNull()) {
+            throw new RuntimeExceptionBuilder(IllegalArgumentException.class)
+                    .developerMessage("The given DTO does not have an Id field; You should supply one.")
+                    .build();
+        }
+        return true;
+    }
+}
--- src/main/java/co/com/hospital/domain/dto/specialty/DetailedSpecialtyDTO.java
+++ src/main/java/co/com/hospital/domain/dto/specialty/DetailedSpecialtyDTO.java
@@ -4,11 +4,13 @@ import co.com.hospital.domain.dto.appointment.PartialAppointmentDTO;
 import co.com.hospital.domain.dto.specialist.PartialSpecialistDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @NoArgsConstructor
 @Getter
+@ToString(callSuper = true)
 public class DetailedSpecialtyDTO extends PartialSpecialtyDTO {
     private PartialSpecialistDTO specialistInCharge;
     private List<PartialAppointmentDTO> associatedAppointments;
