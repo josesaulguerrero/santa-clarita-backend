61133b67c97b81b84177d9e1b9f885ac940f0e94
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-28 07:57:00 -0500

    Id is not necessary on CreateSpecialistDTO

--- src/main/java/co/com/hospital/domain/dto/specialist/CreateSpecialistDTO.java
+++ src/main/java/co/com/hospital/domain/dto/specialist/CreateSpecialistDTO.java
@@ -1,35 +1,14 @@
 package co.com.hospital.domain.dto.specialist;
 
-import co.com.hospital.utils.RuntimeExceptionBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Optional;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class CreateSpecialistDTO {
-    protected Long id;
     protected String dni;
     protected String fullName;
     protected Integer age;
-
-    private boolean idIsNull() {
-        return this.id != null;
-    }
-
-    public boolean isValidDTO() {
-        Optional<String> message = Optional.empty();
-        if (!this.idIsNull()) {
-            message = Optional.of("The given DTO shouldn't have an Id field; It will be automatically assigned by the database.");
-        }
-        if(message.isPresent()) {
-            throw new RuntimeExceptionBuilder(IllegalArgumentException.class)
-                    .developerMessage(message.get())
-                    .build();
-        }
-        return true;
-    }
 }
