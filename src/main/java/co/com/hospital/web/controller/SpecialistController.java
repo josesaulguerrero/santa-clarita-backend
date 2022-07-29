c8d6252aa7ac1f4e840dfce4de5f31760ad25638
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-29 07:45:01 -0500

    Removed try-catch blocks from specialist controller

--- src/main/java/co/com/hospital/web/controller/SpecialistController.java
+++ src/main/java/co/com/hospital/web/controller/SpecialistController.java
@@ -1,13 +1,9 @@
 package co.com.hospital.web.controller;
 
-import co.com.hospital.domain.dto.patient.CreatePatientDTO;
-import co.com.hospital.domain.dto.patient.DetailedPatientDTO;
-import co.com.hospital.domain.dto.patient.PartialPatientDTO;
 import co.com.hospital.domain.dto.specialist.CreateSpecialistDTO;
 import co.com.hospital.domain.dto.specialist.DetailedSpecialistDTO;
 import co.com.hospital.domain.dto.specialist.PartialSpecialistDTO;
 import co.com.hospital.domain.service.SpecialistService;
-import co.com.hospital.utils.HttpException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,46 +19,26 @@ public class SpecialistController {
 
     @GetMapping
     public ResponseEntity<List<PartialSpecialistDTO>> getAll() {
-        try {
-            return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("available")
     public ResponseEntity<List<PartialSpecialistDTO>> getAllAvailable() {
-        try {
-            return new ResponseEntity<>(this.service.findAvaliable(), HttpStatus.OK);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.findAvaliable(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<DetailedSpecialistDTO> getById(@PathVariable("id") Long id) {
-        try {
-            return new ResponseEntity<>(this.service.findById(id), HttpStatus.OK);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.findById(id), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<DetailedSpecialistDTO> post(@RequestBody CreateSpecialistDTO dto) {
-        try {
-            return new ResponseEntity<>(this.service.create(dto), HttpStatus.CREATED);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.create(dto), HttpStatus.CREATED);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<DetailedSpecialistDTO> delete(@PathVariable("id") Long id) {
-        try {
-            return new ResponseEntity<>(this.service.delete(id), HttpStatus.OK);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.delete(id), HttpStatus.OK);
     }
 }
