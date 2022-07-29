a8a5bb7578e1694ef4e32d4ffa1e12d7c6b0eb7c
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-29 07:47:08 -0500

    Removed try-catch blocks from specialty controller

--- src/main/java/co/com/hospital/web/controller/SpecialtyController.java
+++ src/main/java/co/com/hospital/web/controller/SpecialtyController.java
@@ -4,7 +4,6 @@ import co.com.hospital.domain.dto.specialty.CreateAndUpdateSpecialtyDTO;
 import co.com.hospital.domain.dto.specialty.DetailedSpecialtyDTO;
 import co.com.hospital.domain.dto.specialty.PartialSpecialtyDTO;
 import co.com.hospital.domain.service.SpecialtyService;
-import co.com.hospital.utils.HttpException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,55 +19,31 @@ public class SpecialtyController {
 
     @GetMapping
     public ResponseEntity<List<PartialSpecialtyDTO>> getAll() {
-        try {
-            return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<DetailedSpecialtyDTO> getById(@PathVariable("id") Long id) {
-        try {
-            return new ResponseEntity<>(this.service.findById(id), HttpStatus.OK);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.findById(id), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<DetailedSpecialtyDTO> post(@RequestBody CreateAndUpdateSpecialtyDTO dto) {
-        try {
-            return new ResponseEntity<>(this.service.create(dto), HttpStatus.CREATED);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.create(dto), HttpStatus.CREATED);
     }
 
     @PutMapping
     public ResponseEntity<DetailedSpecialtyDTO> update(@RequestBody CreateAndUpdateSpecialtyDTO dto) {
-        try {
-            return new ResponseEntity<>(this.service.update(dto), HttpStatus.ACCEPTED);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.update(dto), HttpStatus.ACCEPTED);
     }
 
     @PutMapping("specialist")
     public ResponseEntity<DetailedSpecialtyDTO> assignNewSpecialist(@RequestBody CreateAndUpdateSpecialtyDTO dto) {
-        try {
-            return new ResponseEntity<>(this.service.assignSpecialist(dto), HttpStatus.ACCEPTED);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.assignSpecialist(dto), HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<DetailedSpecialtyDTO> delete(@PathVariable("id") Long id) {
-        try {
-            return new ResponseEntity<>(this.service.delete(id), HttpStatus.OK);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.delete(id), HttpStatus.OK);
     }
 }
