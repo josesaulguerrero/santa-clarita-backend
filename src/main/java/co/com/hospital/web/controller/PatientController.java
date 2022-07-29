01bb27cdc9fa1281d09487000aacb5241b2cff2e
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-29 07:41:58 -0500

    Removed try-catch blocks from patient controller

--- src/main/java/co/com/hospital/web/controller/PatientController.java
+++ src/main/java/co/com/hospital/web/controller/PatientController.java
@@ -4,7 +4,6 @@ import co.com.hospital.domain.dto.patient.CreatePatientDTO;
 import co.com.hospital.domain.dto.patient.DetailedPatientDTO;
 import co.com.hospital.domain.dto.patient.PartialPatientDTO;
 import co.com.hospital.domain.service.PatientService;
-import co.com.hospital.utils.HttpException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,28 +19,16 @@ public class PatientController {
 
     @GetMapping
     public ResponseEntity<List<PartialPatientDTO>> getAll() {
-        try {
-            return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<DetailedPatientDTO> getById(@PathVariable("id") Long id) {
         return new ResponseEntity<>(this.service.findById(id), HttpStatus.OK);
-        /*try {
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }*/
     }
 
     @PostMapping
     public ResponseEntity<DetailedPatientDTO> post(@RequestBody CreatePatientDTO dto) {
-        try {
-            return new ResponseEntity<>(this.service.create(dto), HttpStatus.CREATED);
-        } catch (HttpException e) {
-            return new ResponseEntity<>(e.getStatusCode());
-        }
+        return new ResponseEntity<>(this.service.create(dto), HttpStatus.CREATED);
     }
 }
