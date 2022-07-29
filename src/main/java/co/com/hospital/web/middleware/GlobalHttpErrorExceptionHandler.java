5f2ef6e7123cdcd63e3bb21761a5e97e0f48d791
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-29 07:39:09 -0500

    Added extra features to HttpExcceptionHandler

--- src/main/java/co/com/hospital/web/middleware/GlobalHttpErrorExceptionHandler.java
+++ src/main/java/co/com/hospital/web/middleware/GlobalHttpErrorExceptionHandler.java
@@ -6,11 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.StreamSupport;
 
 @ControllerAdvice
 public class GlobalHttpErrorExceptionHandler extends ResponseEntityExceptionHandler {
@@ -19,14 +22,30 @@ public class GlobalHttpErrorExceptionHandler extends ResponseEntityExceptionHand
     protected ResponseEntity<Object> handleConflict(HttpException exception, WebRequest request) {
         Map<String, String> responseBody = new HashMap<>();
         responseBody.put("statusCode", exception.getStatusCode().toString());
-        responseBody.put("message", exception.getMessage());
+        responseBody.put("devMessage", exception.getMessage());
         responseBody.put("stackTrace", Arrays.toString(exception.getStackTrace()));
         return handleExceptionInternal(
                 exception,
                 responseBody,
-                new HttpHeaders(),
+                this.mapHeaders(request),
                 exception.getStatusCode(),
                 request
         );
+    }
+
+    private HttpHeaders mapHeaders(WebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        Iterable<String> headerNamesIterable = request::getHeaderNames;
+        List<String> headersNamesList = StreamSupport
+                .stream(headerNamesIterable.spliterator(), false)
+                .toList();
+        headersNamesList
+                .forEach(name ->
+                        headers.put(
+                                name,
+                                Arrays.asList(request.getHeaderValues(name))
+                        ));
+        System.out.println(headers);
+        return headers;
     }
 }
