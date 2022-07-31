018d6a2da51497636e65baec2b9b88756102374b
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-31 11:59:40 -0500

    Refactored the service class for Specialists

--- src/main/java/co/com/hospital/domain/service/SpecialistService.java
+++ src/main/java/co/com/hospital/domain/service/SpecialistService.java
@@ -3,7 +3,9 @@ package co.com.hospital.domain.service;
 import co.com.hospital.persistence.entities.Specialist;
 import co.com.hospital.persistence.entities.Specialty;
 import co.com.hospital.persistence.repository.SpecialistRepository;
+import co.com.hospital.utils.HttpExceptionBuilder;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -24,9 +26,19 @@ public class SpecialistService {
     }
 
     public Specialist findById(Long id) {
+        return this.repository.findById(id)
+                .orElseThrow(() ->
+                        new HttpExceptionBuilder()
+                                .developerMessage("The given id does not belong to any of our registered Specialists")
+                                .statusCode(HttpStatus.NOT_FOUND)
+                                .build()
+                );
     }
 
     public Specialist create(Specialist entity) {
+        entity.setAssociatedSpecialty(null);
+        entity.setIsAvailable(true);
+        return this.repository.save(entity);
     }
 
     private Specialist update(Specialist specialist) {
@@ -34,11 +46,22 @@ public class SpecialistService {
     }
 
     public Specialist assignToSpeciality(Long specialistId, Specialty specialty) {
+        Specialist specialist = this.findById(specialistId);
+        specialist.setAssociatedSpecialty(specialty);
+        specialist.setIsAvailable(false);
+        return this.update(specialist);
     }
 
     public void removeFromSpecialty(Long specialistId) {
+        Specialist specialist = this.findById(specialistId);
+        specialist.setAssociatedSpecialty(null);
+        specialist.setIsAvailable(true);
+        this.update(specialist);
     }
 
     public Specialist delete(Long id) {
+        Specialist specialistToBeDeleted = this.findById(id);
+        this.repository.delete(specialistToBeDeleted);
+        return specialistToBeDeleted;
     }
 }
