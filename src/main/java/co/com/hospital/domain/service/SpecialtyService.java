a0b0b8a0459a1427ccb96ce6fa336a43ec63bbb4
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-31 12:19:49 -0500

    Refactored the service class for Specialty

--- src/main/java/co/com/hospital/domain/service/SpecialtyService.java
+++ src/main/java/co/com/hospital/domain/service/SpecialtyService.java
@@ -1,9 +1,9 @@
 package co.com.hospital.domain.service;
 
-import co.com.hospital.domain.dto.specialty.DetailedSpecialtyDTO;
+import co.com.hospital.persistence.entities.Specialist;
 import co.com.hospital.persistence.entities.Specialty;
-import co.com.hospital.utils.HttpException;
 import co.com.hospital.persistence.repository.SpecialtyRepository;
+import co.com.hospital.utils.HttpExceptionBuilder;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -27,7 +27,10 @@ public class SpecialtyService {
         return this.repository
                 .findById(id)
                 .orElseThrow(
-                        () -> new HttpException("The given id doesn't belong to any Specialty in these facilities.", HttpStatus.NOT_FOUND)
+                        () -> new HttpExceptionBuilder()
+                                .developerMessage("The given id doesn't belong to any Specialty in these facilities.")
+                                .statusCode(HttpStatus.NOT_FOUND)
+                                .build()
                 );
     }
 
@@ -36,14 +39,53 @@ public class SpecialtyService {
     }
 
     public Specialty create(Specialty entity) {
+        Long assignedSpecialistId = entity.getSpecialistInCharge().getId();
+        Specialist assignedSpecialist = this.specialistService.findById(assignedSpecialistId);
+        Specialty specialty = this.repository.save(entity);
+        assignedSpecialist = this.specialistService.assignToSpeciality(assignedSpecialist.getId(), specialty);
+        specialty.setSpecialistInCharge(assignedSpecialist);
+        return specialty;
+    }
+
+    private boolean isValidUpdate(Specialty suppliedEntity, Specialty entityFromDB) {
+        return suppliedEntity.getSpecialistInCharge() == null || !suppliedEntity.getSpecialistInCharge().equals(entityFromDB.getSpecialistInCharge());
     }
 
     public Specialty update(Specialty entity) {
+        Specialty specialty = this.findById(entity.getId());
+        if (isValidUpdate(entity, specialty)) {
+            throw new HttpExceptionBuilder()
+                    .developerMessage("You cannot re-assign a Specialty's specialist directly.")
+                    .statusCode(HttpStatus.FORBIDDEN)
+                    .build();
+        }
+        return this.repository.save(entity);
     }
 
     public Specialty assignSpecialist(Long specialtyId, Long specialistId) {
+        Specialty specialty = this.findById(specialtyId);
+        Long previousSpecialistId = specialty.getSpecialistInCharge().getId();
+        Specialist previousSpecialist = this.specialistService.findById(previousSpecialistId);
+        this.specialistService.removeFromSpecialty(previousSpecialist.getId());
+        Specialist newSpecialist = this.specialistService.findById(specialistId);
+        newSpecialist = this.specialistService.assignToSpeciality(newSpecialist.getId(), specialty);
+        specialty.setSpecialistInCharge(newSpecialist);
+        return this.repository.save(specialty);
+    }
+
+    private boolean specialtyCanBeDeleted(Long specialtyId) {
+        return this.repository.countAssociatedAppointments(specialtyId) == 0;
     }
 
     public Specialty delete(Long id) {
+        Specialty specialtyToBeDeleted = this.findById(id);
+        if (!this.specialtyCanBeDeleted(specialtyToBeDeleted.getId())) {
+            throw new HttpExceptionBuilder()
+                    .developerMessage("This specialty can't be deleted; it has appointments assigned.")
+                    .statusCode(HttpStatus.FORBIDDEN)
+                    .build();
+        }
+        this.repository.delete(specialtyToBeDeleted);
+        return specialtyToBeDeleted;
     }
 }
