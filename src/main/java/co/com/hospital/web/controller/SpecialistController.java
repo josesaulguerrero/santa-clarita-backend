f37edb7203aaa4500fcbd556504ebc805072a74f
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-31 10:42:46 -0500

    Refactored the Specialist controller

--- src/main/java/co/com/hospital/web/controller/SpecialistController.java
+++ src/main/java/co/com/hospital/web/controller/SpecialistController.java
@@ -4,6 +4,8 @@ import co.com.hospital.domain.dto.specialist.CreateSpecialistDTO;
 import co.com.hospital.domain.dto.specialist.DetailedSpecialistDTO;
 import co.com.hospital.domain.dto.specialist.PartialSpecialistDTO;
 import co.com.hospital.domain.service.SpecialistService;
+import co.com.hospital.persistence.entities.Specialist;
+import co.com.hospital.web.mapper.SpecialistMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,29 +19,46 @@ import java.util.List;
 @AllArgsConstructor
 public class SpecialistController {
     private final SpecialistService service;
+    private final SpecialistMapper mapper;
 
     @GetMapping
     public ResponseEntity<List<PartialSpecialistDTO>> getAll() {
-        return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
+        List<PartialSpecialistDTO> DTOs = this.mapper.entitiesToPartialDTOs(
+                this.service.findAll()
+        );
+        return new ResponseEntity<>(DTOs, HttpStatus.OK);
     }
 
     @GetMapping("available")
     public ResponseEntity<List<PartialSpecialistDTO>> getAllAvailable() {
-        return new ResponseEntity<>(this.service.findAvailable(), HttpStatus.OK);
+        List<PartialSpecialistDTO> DTOs = this.mapper.entitiesToPartialDTOs(
+                this.service.findAvailable()
+        );
+        return new ResponseEntity<>(DTOs, HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<DetailedSpecialistDTO> getById(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(this.service.findById(id), HttpStatus.OK);
+        DetailedSpecialistDTO dto = this.mapper.entityToDetailedDTO(
+                this.service.findById(id)
+        );
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<DetailedSpecialistDTO> post(@RequestBody CreateSpecialistDTO dto) {
-        return new ResponseEntity<>(this.service.create(dto), HttpStatus.CREATED);
+        Specialist entityToSave = this.mapper.createDTOToEntity(dto);
+        DetailedSpecialistDTO savedEntityDTO = this.mapper.entityToDetailedDTO(
+                this.service.create(entityToSave)
+        );
+        return new ResponseEntity<>(savedEntityDTO, HttpStatus.CREATED);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<DetailedSpecialistDTO> delete(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(this.service.delete(id), HttpStatus.OK);
+        DetailedSpecialistDTO dto = this.mapper.entityToDetailedDTO(
+                this.service.delete(id)
+        );
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 }
