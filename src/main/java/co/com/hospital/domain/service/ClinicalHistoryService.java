399f8fc1f85ac33c6e031faabcab78140c285507
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-31 11:22:19 -0500

    Refactored the ClinicalHistory service

--- src/main/java/co/com/hospital/domain/service/ClinicalHistoryService.java
+++ src/main/java/co/com/hospital/domain/service/ClinicalHistoryService.java
@@ -4,11 +4,13 @@ import co.com.hospital.persistence.entities.Appointment;
 import co.com.hospital.persistence.entities.ClinicalHistory;
 import co.com.hospital.persistence.entities.Patient;
 import co.com.hospital.persistence.repository.ClinicalHistoryRepository;
-import co.com.hospital.utils.RuntimeExceptionBuilder;
+import co.com.hospital.utils.HttpExceptionBuilder;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -17,14 +19,23 @@ public class ClinicalHistoryService {
     private final ClinicalHistoryRepository repository;
 
     public ClinicalHistory findById(Long id) {
+        return this.repository
+                .findById(id)
+                .orElseThrow(() ->
+                        new HttpExceptionBuilder()
+                                .developerMessage("The given id doesn't belong to any Clinical History.")
+                                .statusCode(HttpStatus.NOT_FOUND)
+                                .build()
+                );
     }
 
     public ClinicalHistory findByPatientId(Long patientId) {
         return this.repository
                 .findByAssociatedPatientId(patientId)
                 .orElseThrow(
-                        () -> new RuntimeExceptionBuilder(IllegalArgumentException.class)
+                        () -> new HttpExceptionBuilder()
                                 .developerMessage("The given patient id doesn't belong to any Clinical History.")
+                                .statusCode(HttpStatus.NOT_FOUND)
                                 .build()
                 );
     }
@@ -33,12 +44,17 @@ public class ClinicalHistoryService {
         return this.repository.existsById(id);
     }
 
-    public ClinicalHistory create() {
+    public ClinicalHistory create(Patient patient) {
+        ClinicalHistory clinicalHistory = new ClinicalHistory(List.of(), patient);
+        return this.repository.save(clinicalHistory);
     }
 
-    public void assignPatient(Long clinicalHistoryId, Patient patient) {
-    }
+    /*public void assignPatient(Long clinicalHistoryId, Patient patient) {
+    }*/
 
     public ClinicalHistory addAppointmentRecord(Long clinicalHistoryId, Appointment appointment) {
+        ClinicalHistory clinicalHistory = this.findById(clinicalHistoryId);
+        clinicalHistory.addAppointment(appointment);
+        return this.repository.save(clinicalHistory);
     }
 }
