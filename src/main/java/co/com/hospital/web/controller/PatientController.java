10ba8b4e1bd2351ce6b6145304d3f9a01fc04780
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-31 10:28:12 -0500

    Refactored the Patient controller

--- src/main/java/co/com/hospital/web/controller/PatientController.java
+++ src/main/java/co/com/hospital/web/controller/PatientController.java
@@ -4,6 +4,8 @@ import co.com.hospital.domain.dto.patient.CreatePatientDTO;
 import co.com.hospital.domain.dto.patient.DetailedPatientDTO;
 import co.com.hospital.domain.dto.patient.PartialPatientDTO;
 import co.com.hospital.domain.service.PatientService;
+import co.com.hospital.persistence.entities.Patient;
+import co.com.hospital.web.mapper.PatientMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,19 +19,26 @@ import java.util.List;
 @CrossOrigin("*")
 public class PatientController {
     private final PatientService service;
+    private final PatientMapper mapper;
 
     @GetMapping
     public ResponseEntity<List<PartialPatientDTO>> getAll() {
-        return new ResponseEntity<>(this.service.findAll(), HttpStatus.OK);
+        List<PartialPatientDTO> mappedPatients = this.mapper.entitiesToPartialDTOs(this.service.findAll());
+        return new ResponseEntity<>(mappedPatients, HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<DetailedPatientDTO> getById(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(this.service.findById(id), HttpStatus.OK);
+        DetailedPatientDTO mappedPatient = this.mapper.entityToDetailedDTO(this.service.findById(id));
+        return new ResponseEntity<>(mappedPatient, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<DetailedPatientDTO> post(@RequestBody CreatePatientDTO dto) {
-        return new ResponseEntity<>(this.service.create(dto), HttpStatus.CREATED);
+        Patient entityToSave = this.mapper.createDTOToEntity(dto);
+        DetailedPatientDTO mappedPatient = this.mapper.entityToDetailedDTO(
+                this.service.create(entityToSave)
+        );
+        return new ResponseEntity<>(mappedPatient, HttpStatus.CREATED);
     }
 }
