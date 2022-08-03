c764b913d6aab58e7894f7c4bbda7b203e5eb62e
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-03 17:52:25 -0500

    Made HttpExceptionHandler extend from ApplicationExceptionHandler

--- src/main/java/co/com/hospital/web/middleware/HttpExceptionHandler.java
+++ src/main/java/co/com/hospital/web/middleware/HttpExceptionHandler.java
@@ -2,53 +2,32 @@ package co.com.hospital.web.middleware;
 
 import co.com.hospital.utils.HttpException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.StreamSupport;
 
-@ControllerAdvice(assignableTypes = {HttpException.class})
+@ControllerAdvice
 @Slf4j
-public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
+public class HttpExceptionHandler extends ApplicationExceptionHandler<HttpException> {
     @ExceptionHandler(value = {HttpException.class})
     protected ResponseEntity<Object> handleConflict(HttpException exception, WebRequest request) {
         Map<String, String> responseBody = new HashMap<>();
         responseBody.put("statusCode", exception.getStatusCode().toString());
         responseBody.put("devMessage", exception.getMessage());
         responseBody.put("stackTrace", Arrays.toString(exception.getStackTrace()));
-        String stackTrace = ExceptionUtils.getStackTrace(exception);
-        logger.error(stackTrace);
+        super.logStackTrace(exception);
+
         return handleExceptionInternal(
                 exception,
                 responseBody,
-                this.mapHeaders(request),
+                super.mapHeaders(request),
                 exception.getStatusCode(),
                 request
         );
-    }
-
-    private HttpHeaders mapHeaders(WebRequest request) {
-        HttpHeaders headers = new HttpHeaders();
-        Iterable<String> headerNamesIterable = request::getHeaderNames;
-        List<String> headersNamesList = StreamSupport
-                .stream(headerNamesIterable.spliterator(), false)
-                .toList();
-        headersNamesList
-                .forEach(name ->
-                        headers.put(
-                                name,
-                                Arrays.asList(request.getHeaderValues(name))
-                        ));
-        headers.put("Content-Type", List.of("application/json"));
-        return headers;
     }
 }
