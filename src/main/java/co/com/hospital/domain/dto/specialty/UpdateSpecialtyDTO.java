a86877275059ba408a088b1686c3743334645fe1
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-03 17:22:15 -0500

    Added dto validations on UpdateSpecialty and removed unused imports from CreateSpecialtyDTO

--- src/main/java/co/com/hospital/domain/dto/specialty/UpdateSpecialtyDTO.java
+++ src/main/java/co/com/hospital/domain/dto/specialty/UpdateSpecialtyDTO.java
@@ -2,6 +2,9 @@ package co.com.hospital.domain.dto.specialty;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(callSuper = true)
@@ -9,5 +12,7 @@ import lombok.*;
 @Getter
 @Setter
 public class UpdateSpecialtyDTO extends CreateSpecialtyDTO {
+    @NotNull(message = "The id of the Specialty you want to update cannot be null")
+    @Positive(message = "You cannot supply a negative or zero value for the id of the Specialty.")
     private Long id;
 }
