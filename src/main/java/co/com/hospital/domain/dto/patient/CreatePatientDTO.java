e722998888d0a282499b6693af109e185bb75420
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-03 17:14:54 -0500

    Added dto validations on CreatePatient

--- src/main/java/co/com/hospital/domain/dto/patient/CreatePatientDTO.java
+++ src/main/java/co/com/hospital/domain/dto/patient/CreatePatientDTO.java
@@ -2,11 +2,22 @@ package co.com.hospital.domain.dto.patient;
 
 import lombok.*;
 
+import javax.validation.constraints.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class CreatePatientDTO {
+    @NotNull(message = "A patient's DNI cannot be null.")
+    @NotBlank(message = "A patient's DNI cannot be an empty string.")
     private String dni;
+
+    @NotNull(message = "A patient's name cannot be null.")
+    @NotBlank(message = "A patient's name cannot be an empty string.")
     private String fullName;
+
+    @NotNull(message = "You cannot supply a nullable value for the age of a patient.")
+    @Positive(message = "You cannot supply a negative or zero value for the age of a patient.")
+    @Max(value = 115, message = "The age of a patient is over 115? Are you kidding me?")
     private Integer age;
 }
