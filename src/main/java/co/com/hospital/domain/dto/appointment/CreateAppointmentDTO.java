b70d51eea4d50e3a4e35ab23c1c4a154a79ffc18
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-03 17:14:21 -0500

    Added dto validations on CreateAppointment

--- src/main/java/co/com/hospital/domain/dto/appointment/CreateAppointmentDTO.java
+++ src/main/java/co/com/hospital/domain/dto/appointment/CreateAppointmentDTO.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class CreateAppointmentDTO {
+    @NotNull(message = "You cannot supply a nullable value for the patient Id.")
+    @Positive(message = "You cannot supply a negative or zero value for the patient Id.")
     private Long patientId;
+
+    @NotNull(message = "You cannot supply a nullable value for the specialty Id.")
+    @Positive(message = "You cannot supply a negative or zero value for the specialty Id.")
     private Long specialtyId;
 }
