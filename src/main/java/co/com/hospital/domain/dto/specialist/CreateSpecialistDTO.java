2a000912f1622bb9d9bbf7d99aa28bb6712b6b3c
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-03 17:17:27 -0500

    Added dto validations on CreateSpecialist

--- src/main/java/co/com/hospital/domain/dto/specialist/CreateSpecialistDTO.java
+++ src/main/java/co/com/hospital/domain/dto/specialist/CreateSpecialistDTO.java
@@ -4,11 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class CreateSpecialistDTO {
-    protected String dni;
-    protected String fullName;
-    protected Integer age;
+    @NotNull(message = "A specialist's DNI cannot be null.")
+    @NotBlank(message = "A specialist's DNI cannot be an empty string.")
+    private String dni;
+
+    @NotNull(message = "The name of a specialist cannot be null.")
+    @NotBlank(message = "The name of a specialist cannot be an empty string.")
+    private String fullName;
+
+    @NotNull(message = "The age of a specialist cannot be null.")
+    @Positive(message = "You cannot supply a negative or zero value for the age of a patient.")
+    @Max(value = 75, message = "People over 75 are not allowed to work in this hospital.")
+    private Integer age;
 }
